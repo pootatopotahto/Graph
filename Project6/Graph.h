e97e23021a26617c88218992587c4e182b2ea26f
liloozmo <osmo_t1@denison.edu>
2024-04-10 00:14:28 -0400

    little update

--- Project6/Graph.h
+++ Project6/Graph.h
@@ -33,16 +33,16 @@ public:
     virtual void    insertEdge      (int v1, int v2, int weight) {}
     // BFS based algorithms
     virtual void BFS ( int source );
-    virtual void printBFSTable ( int source );
-    virtual void printBFSPath ( int s, int d );
-    virtual void printMostDistant ( int s );
-    virtual bool isConnected ( void );
+    void printBFSTable ( int source );
+    void printBFSPath ( int s, int d );
+    void printMostDistant ( int s );
+    bool isConnected ( void );
 // DFS based algorithms
     virtual void DFS ( void );
     virtual void DFS_Visit ( int v, int &clock );
-    virtual void printDFSTable ( void );
-    virtual void printTopologicalSort ( void );
-    virtual void printDFSParenthesization( void );
+    void printDFSTable ( void );
+    void printTopologicalSort ( void );
+    void printDFSParenthesization( void );
     virtual void classifyDFSEdges ( void );
     virtual void indexSort ( int a[] )
 
